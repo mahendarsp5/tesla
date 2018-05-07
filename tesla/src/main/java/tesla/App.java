e69d92d60a5bad32f8207017d8a6e7c52d122122
mahendar <mahendar.sp5@gmail.com>
2018-05-07 18:29:04 +0530

    initial commit

---
+++ tesla/src/main/java/tesla/App.java
@@ -0,0 +1,8 @@
+package tesla;
+
+public class App {
+	public String Sample() {
+	return "sample program";
+
+}
+}
---
+++ tesla/src/test/java/tesla/Apptest.java
@@ -0,0 +1,13 @@
+package tesla;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class Apptest {
+
+	@Test
+	public void test() {
+		}
+
+}
